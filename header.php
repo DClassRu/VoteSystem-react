02c4b34d9f455a8a28fa1ec5cbe0b1a334e3ca18
L1rz <contact@rzx.lol>
2024-02-01 15:56:40 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Система голосования</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <i class="fa-solid fa-check-to-slot"> </i><a class="navbar-brand">Система голосования</a>
+    </nav>
+
+    <div class="container mt-5">
